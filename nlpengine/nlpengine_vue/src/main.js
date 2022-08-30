9acf6f1693858f2350d8dfaa52d63029181c7985
s3818520 <57244573+s3818520@users.noreply.github.com>
2022-08-30 17:03:30 +0700

    add sidebar

--- nlpengine/nlpengine_vue/src/main.js
+++ nlpengine/nlpengine_vue/src/main.js
@@ -5,10 +5,13 @@ import router from './router'
 import store from './store'
 import VChartkick from 'vue-chartkick'
 import 'chartkick/chart.js'
+import '@fortawesome/fontawesome-free/js/all'
+import HighlightableInput from "vue-highlightable-input"
 
 let app = createApp(App);
 app.use(VChartkick);
 app.use(router);
+app.use(HighlightableInput)
 app.use(store);
 app.mount('#app')
 
